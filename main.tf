37d2013b59fa6d0eb580c8d84e28ba4c58d333d5
Tayfun <typhoon9721@gmail.com>
2019-06-10 13:12:39 +0300

    added public s3 bucket

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-test1-bucket"
+  acl    = "private"
+  policy = "${file("publicPolicy.json")}"
+
+}
